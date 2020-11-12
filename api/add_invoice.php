92080f26d0b4ab110217fb5a1465a16bf33aeb09
mackliu <mackliu@gmail.com>
2020-11-12 15:49:54 +0800

    完成發票新增功能及資料表建置

--- api/add_invoice.php
+++ api/add_invoice.php
@@ -2,5 +2,18 @@
 //撰寫新增消費發票的程式碼
 //將發票的號碼及相關資訊寫入資料庫
 
+$dsn="mysql:host=localhost;dbname=invoice;charset=utf8";
+$pdo=new PDO($dsn,'root','');
 
+echo "<pre>";
+print_r(array_keys($_POST));
+echo "</pre>";
+
+
+$sql="insert into invoices (`".implode("`,`",array_keys($_POST))."`) values('".implode("','",$_POST)."')";
+echo $sql;
+$pdo->exec($sql);
+
+echo "新增完成";
+header("location:../index.php");
 ?>
--- main.php
+++ main.php
@@ -29,7 +29,7 @@
 
     <div class="col-8 d-flex justify-content-between p-3 mx-auto border">
         <form action="api/add_invoice.php" method="post">
-            <div>年份:<input type="text" name="year"></div>
+            <div>日期:<input type="date" name="date"></div>
             期別:<select name="period">
                 <option value="1">1,2</option>
                 <option value="2">3,4</option>
@@ -39,8 +39,8 @@
                 <option value="6">11,12</option>
             </select>
             <div>發票號碼:
-                <input type="text" name="prepend" style="width:50px">
-                <input type="text" name="number" style="width:150px">
+                <input type="text" name="code" style="width:50px">
+                <input type="number" name="number" style="width:150px">
             </div>
             <div>
                 發票金額:<input type="number" name="payment">
