bf5572f82fe5630682150d5f04e8b6458a70152f
mackliu <mackliu@gmail.com>
2020-11-12 16:25:30 +0800

    將PDO連線移出至base.php檔案

--- api/add_invoice.php
+++ api/add_invoice.php
@@ -1,9 +1,8 @@
 <?php
 //撰寫新增消費發票的程式碼
 //將發票的號碼及相關資訊寫入資料庫
+include_once "../base.php";
 
-$dsn="mysql:host=localhost;dbname=invoice;charset=utf8";
-$pdo=new PDO($dsn,'root','');
 
 echo "<pre>";
 print_r(array_keys($_POST));
@@ -15,5 +14,5 @@ echo $sql;
 $pdo->exec($sql);
 
 echo "新增完成";
-header("location:../index.php");
+//header("location:../index.php");
 ?>
---
+++ base.php
@@ -0,0 +1,9 @@
+<?php
+
+$dsn="mysql:host=localhost;dbname=invoice;charset=utf8";
+$pdo=new PDO($dsn,'root','');
+
+date_default_timezone_set("Asia/Taipei");
+session_start();
+
+?>
