36a7a7468a860100558bb02200e877fa44c708af
mackliu <mackliu@gmail.com>
2020-11-13 09:43:06 +0800

    美化發票清單的畫面及增加要顯示的欄位內容

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 <h3 class="text-center">統一發票紀錄與對獎</h3>
 
 <div class="container">
-    <div class="col-8 d-flex justify-content-between p-3 mx-auto border">
+    <div class="col-lg-8 col-md-12 d-flex justify-content-between p-3 mx-auto border">
     <?php
         $month=[
             1=>"1,2月",
@@ -42,7 +42,7 @@
         </div>
     </div>
 
-    <div class="col-8 d-flex p-3 mx-auto border">
+    <div class="col-lg-8 col-md-12  d-flex p-3 mx-auto border">
     <?php
 
         if(isset($_GET['do'])){
--- invoice_list.php
+++ invoice_list.php
@@ -1,12 +1,32 @@
 <?php
 include_once "base.php";
 
-$sql="select * from `invoices`";
+$sql="select * from `invoices` order by date desc";
 
 $rows=$pdo->query($sql)->fetchAll();
 
-foreach($rows as $row){
-echo $row['code'].$row['number']."<br>";
-}
-
 ?>
+
+<table class="table text-center">
+    <tr>
+        <td>發票號碼</td>
+        <td>消費日期</td>
+        <td>消費金額</td>
+        <td>操作</td>
+    </tr>
+    <?php
+    foreach($rows as $row){
+    ?>
+    <tr>
+        <td><?=$row['code'].$row['number'];?></td>
+        <td><?=$row['date'];?></td>
+        <td><?=$row['payment'];?></td>
+        <td>
+            <button class="btn btn-sm btn-primary">編輯</button>
+            <button class="btn btn-sm btn-danger">刪除</button>
+        </td>
+    </tr>
+    <?php
+    }
+    ?>
+</table>
