6174a0c86c8ef1be4cdf3bc23ea66b135812b585
mackliu <mackliu@gmail.com>
2020-11-13 13:46:50 +0800

    設計開獎獎號紀錄表單

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
             <a href="">對獎</a>
         </div>
         <div class="text-center">
-            <a href="">輸入獎號</a>
+            <a href="?do=add_awards">輸入獎號</a>
         </div>
         <div class="text-center">
             <a href="index.php">回首頁</a>
