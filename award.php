9e135bc491ad3abf0e42ce024daeb8e826cfdedf
mackliu <mackliu@gmail.com>
2020-11-18 14:14:08 +0800

    完成對單張發票的對獎做法

--- award.php
+++ award.php
@@ -23,13 +23,59 @@ $date=$invoice['date'];
 // $period=ceil($month/2)
 $year=explode('-',$date)[0];
 $period=ceil(explode('-',$date)[1]/2);
-echo "select * from award_numbers where year='$year' && period='$period'";
+//echo "select * from award_numbers where year='$year' && period='$period'";
 $awards=$pdo->query("select * from award_numbers where year='$year' && period='$period'")->fetchALL();
 
-
+/* 
 echo "<pre>";
 print_r($awards);
-echo "</pre>";
+echo "</pre>"; */
+
+foreach($awards as $award){
+    switch($award['type']){
+        case 1:
+            //特別號=我的發票號碼
+            
+            
+            if($award['number']==$number){
+                echo "<br>號碼=".$number."<br>";
+                echo "<br>中了特別獎<br>";
+            }else{
+                echo "<br>特別獎沒中<br>";
+            }
+        break;
+        case 2:
+            
+            if($award['number']==$number){
+                echo "<br>號碼=".$number."<br>";
+                echo "中了特獎<br>";
+            }else{
+                echo "特獎沒中<br>";
+            }
+
+        break;
+        case 3:
+            for($i=5;$i>=0;$i--){
+                $target=mb_substr($award['number'],$i,(8-$i),'utf8');
+                $mynumber=mb_substr($number,$i,(8-$i),'utf8');
+
+                if($target==$mynumber){
+                    echo "<br>號碼=".$number."<br>";
+                    echo "中了{$awardStr[$i]}獎<br>";
+                }else{
+                    break;
+                    //continue
+                }
+            }
+        break;
+        case 4:
+            if($award['number']==mb_substr($number,5,3,'utf8')){
+                echo "<br>號碼=".$number."<br>";
+                echo "中了增開六獎";
+            }
+        break;
+    }
+}
 
 
 ?>
--- base.php
+++ base.php
@@ -6,4 +6,6 @@ $pdo=new PDO($dsn,'root','');
 date_default_timezone_set("Asia/Taipei");
 session_start();
 
+$awardStr=['頭','二','三','四','五','六'];
+
 ?>
