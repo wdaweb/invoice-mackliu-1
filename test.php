d016e6ed8f2af21bc89d13f8bd8068f589e87604
mackliu <mackliu@gmail.com>
2020-11-20 15:51:17 +0800

    增加刪除指定id或條件的函式

--- test.php
+++ test.php
@@ -23,39 +23,51 @@ function find($table,$id){
 
 function all($table,...$arg){
     global $pdo;
-
-    //echo gettype($arg);
-
     $sql="select * from $table ";
 
     if(isset($arg[0])){
+    
         if(is_array($arg[0])){
             //製作會在 where 後面的句子字串(陣列格式)
-            if(!empty($arg[0])){
                 foreach($arg[0] as $key => $value){
                     $tmp[]=sprintf("`%s`='%s'",$key,$value);
-                    //$tmp[]="`".$key."`='".$value."'";
                 }
 
                 $sql=$sql." where ".implode(' && ',$tmp);
-            }
-
+            
         }else{
             //製作非陣列的語句接在$sql後面
                 $sql=$sql.$arg[0];       
         }
     }
-    
+
     if(isset($arg[1])){
-
         $sql=$sql.$arg[1];
-
     }
     echo $sql."<br>";
     return $pdo->query($sql)->fetchAll(); 
-
 }
 
+function del($table,$id){
+    global $pdo;
+    $sql="delete from $table where ";
+    if(is_array($id)){
+        foreach($id as $key => $value){
+            $tmp[]=sprintf("`%s`='%s'",$key,$value);
+            
+        }
+        $sql=$sql.implode(' && ',$tmp);
+    }else{
+        $sql=$sql . " id='$id' ";
+    }
+    //echo $sql;
+    $row=$pdo->exec($sql);
+
+    return $row;
+}
+
+$def=['code'=>'GD'];
+echo del('invoices',$def);
 
 /* echo "<hr>";
 print_r(all('invoices'));
@@ -66,13 +78,13 @@ print_r(all('invoices',['code'=>"AB",'period'=>1])," order by date desc");
 echo "<hr>";
 print_r(all('invoices'," limit 5"));
  */
-echo "<hr>";
+/* echo "<hr>";
 all('invoices');
 echo "<hr>";
 all('invoices',['code'=>"GD",'period'=>6]);
 echo "<hr>";
 all('invoices',['code'=>"AB",'period'=>1]," order by date desc");
 echo "<hr>";
-all('invoices'," limit 5");
+all('invoices'," limit 5"); */
 
 ?>
